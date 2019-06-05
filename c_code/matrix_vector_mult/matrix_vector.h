d0dd4e904bdaeb2d45dfa1445058208967495d00
MohamedElgammal <mohamed.elgammal@mail.utoronto.ca>
2019-06-05 20:03:49 +0000

    update function definition

--- c_code/matrix_vector_mult/matrix_vector.h
+++ c_code/matrix_vector_mult/matrix_vector.h
@@ -4,15 +4,18 @@
 typedef float dataType;
 
 void mv_input(dataType * mem,            // global memory pointer
-              int input_offset,       // offset of inputs
+              int input1_offset,       // offset of input A
+			  int input2_offset,       // offset of input B
               int output_offset      // offset of outputs
 );
 void mv_output(dataType * mem,            // global memory pointer
-               int input_offset,       // offset of inputs
+               int input1_offset,       // offset of inputs
+			   int input2_offset,       // offset of input B
                int output_offset      // offset of outputs
 );
 void mv_state(dataType * mem,            // global memory pointer
-              int input_offset,       // offset of inputs
+              int input1_offset,       // offset of inputs
+			  int input2_offset,       // offset of input B
               int output_offset      // offset of outputs
 );
 
