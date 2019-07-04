df52a471ac19bae74d5a10424fbdc240d3aa6782
ChenxiTang <44796503+ChenxiTang@users.noreply.github.com>
2019-07-04 16:34:23 -0400

    clean up

--- hls_proj/LSTM_proj/solution1/csim/build/obj/mv_output.d
+++
@@ -1,2 +0,0 @@
-obj/mv_output.o: ../../../../../c_code/matrix_vector_mult/mv_output.cpp \
- ../../../../../c_code/matrix_vector_mult/matrix_vector.h
--- hls_proj/LSTM_proj/solution1/csim/build/obj/mv_state.d
+++
@@ -1,2 +0,0 @@
-obj/mv_state.o: ../../../../../c_code/matrix_vector_mult/mv_state.cpp \
- ../../../../../c_code/matrix_vector_mult/matrix_vector.h
